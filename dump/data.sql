72980b0b26d0e87b7150532f3dd11c346799bf54
Ilario Pierbattista <pierbattista.ilario@gmail.com>
2015-04-09 11:18:50 +0200

    Schema

---
+++ dump/data.sql
@@ -0,0 +1,3 @@
+Insert Into Cliente (CF_PIVA, Nome, Cognome, Citta, Via, Civico, CAP, NDocId)
+	Values ('PRBLRI93R05I324O', 'Ilario', 'Pierbattista', 'Monte Urano',
+		'Via Picena', '14', '63813', 'AO5254936');
